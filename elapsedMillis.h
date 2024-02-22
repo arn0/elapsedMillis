1598f7c476d19293b2470619e2be3856eb59e5c5
arn0 <arnoc@xs4all.nl>
2024-02-22 16:18:53 +0100

    Remove all references to arduino

--- elapsedMillis.h
+++ elapsedMillis.h
@@ -25,19 +25,19 @@
 #define elapsedMillis_h
 #ifdef __cplusplus
 
-#include "Arduino.h"
+#include "esp_timer.h"
 
 class elapsedMicros
 {
 private:
 	unsigned long us;
 public:
-	elapsedMicros(void) { us = micros(); }
-	elapsedMicros(unsigned long val) { us = micros() - val; }
+	elapsedMicros(void) { us = esp_timer_get_time(); }
+	elapsedMicros(unsigned long val) { us = esp_timer_get_time() - val; }
 	elapsedMicros(const elapsedMicros &orig) { us = orig.us; }
-	operator unsigned long () const { return micros() - us; }
+	operator unsigned long () const { return esp_timer_get_time() - us; }
 	elapsedMicros & operator = (const elapsedMicros &rhs) { us = rhs.us; return *this; }
-	elapsedMicros & operator = (unsigned long val) { us = micros() - val; return *this; }
+	elapsedMicros & operator = (unsigned long val) { us = esp_timer_get_time() - val; return *this; }
 	elapsedMicros & operator -= (unsigned long val)      { us += val ; return *this; }
 	elapsedMicros & operator += (unsigned long val)      { us -= val ; return *this; }
 	elapsedMicros operator - (int val) const           { elapsedMicros r(*this); r.us += val; return r; }
@@ -55,12 +55,12 @@ class elapsedMillis
 private:
 	unsigned long ms;
 public:
-	elapsedMillis(void) { ms = millis(); }
-	elapsedMillis(unsigned long val) { ms = millis() - val; }
+	elapsedMillis(void) { ms = esp_timer_get_time() / 1000ULL; }
+	elapsedMillis(unsigned long val) { ms = (esp_timer_get_time() / 1000ULL) - val; }
 	elapsedMillis(const elapsedMillis &orig) { ms = orig.ms; }
-	operator unsigned long () const { return millis() - ms; }
+	operator unsigned long () const { return (esp_timer_get_time() / 1000ULL) - ms; }
 	elapsedMillis & operator = (const elapsedMillis &rhs) { ms = rhs.ms; return *this; }
-	elapsedMillis & operator = (unsigned long val) { ms = millis() - val; return *this; }
+	elapsedMillis & operator = (unsigned long val) { ms = (esp_timer_get_time() / 1000ULL) - val; return *this; }
 	elapsedMillis & operator -= (unsigned long val)      { ms += val ; return *this; }
 	elapsedMillis & operator += (unsigned long val)      { ms -= val ; return *this; }
 	elapsedMillis operator - (int val) const           { elapsedMillis r(*this); r.ms += val; return r; }
@@ -78,12 +78,12 @@ class elapsedSeconds
 private:
 	unsigned long s;
 public:
-	elapsedSeconds(void) { s = millis()/1000; }
-	elapsedSeconds(unsigned long val) { s = millis()/1000 - val; }
+	elapsedSeconds(void) { s = (esp_timer_get_time() / 1000ULL)/1000; }
+	elapsedSeconds(unsigned long val) { s = (esp_timer_get_time() / 1000ULL)/1000 - val; }
 	elapsedSeconds(const elapsedSeconds &orig) { s = orig.s; }
-	operator unsigned long () const { return millis()/1000 - s; }
+	operator unsigned long () const { return (esp_timer_get_time() / 1000ULL)/1000 - s; }
 	elapsedSeconds & operator = (const elapsedSeconds &rhs) { s = rhs.s; return *this; }
-	elapsedSeconds & operator = (unsigned long val) { s = millis()/1000 - val; return *this; }
+	elapsedSeconds & operator = (unsigned long val) { s = (esp_timer_get_time() / 1000ULL)/1000 - val; return *this; }
 	elapsedSeconds & operator -= (unsigned long val)      { s += val ; return *this; }
 	elapsedSeconds & operator += (unsigned long val)      { s -= val ; return *this; }
 	elapsedSeconds operator - (int val) const           { elapsedSeconds r(*this); r.s += val; return r; }
